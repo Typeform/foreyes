eed4caf9551ba80e9c484a0cb9bc37e2ddf77900
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-05-22 18:25:02 +0200

    merge master onto refactor_createConfigurationFiles

--- src/getAllExamplePages.spec.js
+++ src/getAllExamplePages.spec.js
@@ -4,7 +4,7 @@ jest.mock("fs")
 describe("getAllExamplePages", () => {
     describe("given a path to the components' folder", () => {
         it("returns a js-readable list of component paths and combination paths", () => {
-            const input = require("./__fixtures__/componentList");
+            const input = require("./__fixtures__/exampleList");
             require("fs").__fileExists = true;
             const output = getAllExamplePages(input);
             const expected = `const examples = {
