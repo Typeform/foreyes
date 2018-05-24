ed063768899cfb319aca4bd84997d9ffb4a59843
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-05-24 17:31:18 +0200

    erased useless comment in mass-generate-combinations

--- scripts/mass-generate-combinations.js
+++ scripts/mass-generate-combinations.js
@@ -1,4 +1,3 @@
 const generateAllCombinations = require('../src/generateAllCombinations')
 
-const result = generateAllCombinations()
-console.log(`\n${result.notCreated}/${result.nComponents} components with no propTypes (${(100 * result.notCreated / result.nComponents).toFixed(2)}%)`)
+generateAllCombinations()
