9a78f7b088597f9b3f25c8258a8bc7603c6995f6
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-05-24 16:37:17 +0200

    revert to using explicit requires

--- src/getAllExamplePages.spec.js
+++ src/getAllExamplePages.spec.js
@@ -1,13 +1,19 @@
 const getAllExamplePages = require("./getAllExamplePages");
 
 jest.mock("fs")
+jest.mock("../katt.config")
 describe("getAllExamplePages", () => {
     describe("given a path to the components' folder", () => {
         it("returns a js-readable list of component paths and combination paths", () => {
             const input = require("./__fixtures__/exampleList");
             require("fs").__fileExists = true;
             const output = getAllExamplePages(input);
-            const expected = `const examples = ["button","input","headline","label"];
+            const expected = `const examples = {
+button: { component: require('../src/button').default, combinations: require('../test/button.exampleCombinations.js').default },
+input: { component: require('../src/input').default, combinations: require('../test/input.exampleCombinations.js').default },
+headline: { component: require('../src/headline').default, combinations: require('../test/headline.exampleCombinations.js').default },
+label: { component: require('../src/label').default, combinations: require('../test/label.exampleCombinations.js').default }
+};
 export default examples;`;
             expect(output.replace(/ |\t/g,"")).toMatch(expected.replace(/ |\t/g,""));
         })
