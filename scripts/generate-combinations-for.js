f56a7b668589abda9c6c3e5ecfc65cadcee54365
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-07-11 19:43:34 +0200

    refactor: delete scripts that cannot be called from outside the package

--- scripts/generate-combinations-for.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const generateCombinationsFor = require(path.resolve(__dirname, '../src/examples/generateCombinationsFor'))
-const getComponentFromStdin = require(path.resolve(__dirname, '../src/getComponentFromStdin'))
-
-const componentName = getComponentFromStdin()
-const result = generateCombinationsFor(componentName)
-console.log(result)
