48bbdbc81f6ac3adb072bd85150051227577a59c
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-08-02 17:39:02 +0200

    feat(Functional Tests): Add functional testing boilerplate

---
+++ tests/katt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  path_to_components: 'src/components/',
+  path_to_examples: 'tests/',
+  component_folder_blacklist: ['__mocks__'],
+  misMatchTolerance: 0,
+  ignoreComparison: 'antialiasing',
+  viewports: [
+    {
+      width: 1024,
+      height: 600
+    }
+  ]
+}
---
+++ tests/tests/button.exampleCombinations.js
@@ -0,0 +1,5 @@
+exports.default = {
+  color: ['red', 'blue', 'green'],
+  size: ['big', 'small', 'medium'],
+  children: ['Click me!', 'Click me, too!', 'Accept', 'Reject']
+}
