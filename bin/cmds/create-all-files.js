3d18ad7695d7cc2f0755c3c7d39ae726d69dc0a4
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-09-27 16:04:46 +0200

    refactor: create-file and create-all-files

--- bin/cmds/create-all-files.js
+++ bin/cmds/create-all-files.js
@@ -2,7 +2,7 @@ exports.command = 'create-all-files'
 exports.desc = 'Create a base example file for all found components'
 exports.handler = () => {
   const path = require('path')
-  const generateAllCombinations = require(path.resolve(
+  const generateAllCombinations = require(path.join(
     __dirname,
     '../../src/examples/generateAllCombinations'
   ))
