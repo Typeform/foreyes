e4d2b1fbd9fc99b7fe00a79743cfe7b977b0ecbb
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-09-28 17:10:28 +0200

    feat(report): report to display date of generation

--- report/src/index.js
+++ report/src/index.js
@@ -2,4 +2,4 @@ import Report from './report.js'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<Report data={report} />, document.getElementById('root'))
+ReactDOM.render(<Report data={report} reportDate={reportDate} />, document.getElementById('root'))
